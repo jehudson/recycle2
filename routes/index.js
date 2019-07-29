beb2bb750dd183fc7b8e1720fdb7eec0bd2c27cc
John Hudson <john@jehudson.me>
2019-07-29 19:06:54 +0100

    change alerts

--- routes/index.js
+++ routes/index.js
@@ -155,7 +155,7 @@ router.get('/respond_post/:id', isAuthenticated, function(req, res) {
 });
 
 
-router.post('/email_alerts/:id', isAuthenticated, function(req, res, done) {
+router.post('/post_enquiry/:id', isAuthenticated, function(req, res, done) {
   posts.findOne({_id : req.params.id}, function (err, posts) {
 
     var post_id = req.params.id
@@ -180,7 +180,7 @@ router.post('/email_alerts/:id', isAuthenticated, function(req, res, done) {
 
 });
 
-router.post('/post_enquiry/:id', isAuthenticated, function(req, res, done) {
+router.post('/email_alerts/:id', isAuthenticated, function(req, res, done) {
   posts.findOne({_id : req.params.id}, function (err, posts) {
     User.find({"email_alerts": "on"}, ).then(function(users) {
       users.forEach(function(user) {
